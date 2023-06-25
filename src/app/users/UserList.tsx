0aa35d5d7aa34d0353e6cb9d42605ac1a74f67ca
maeta187 <nexus18789@gmail.com>
2023-06-25 16:13:14 +0900

    ✨ : UserListの実装

---
+++ src/app/users/UserList.tsx
@@ -0,0 +1,24 @@
+type User = {
+  id: string
+  name: string
+  email: string
+}
+
+/**
+ * Server Componentはサーバー側でfetchを実行するのでconsole.logがサーバー側で実行される
+ * そのため、ブラウザのconsoleには出力されない
+ */
+const UserList = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 5000))
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const users: User[] = await response.json()
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  )
+}
+
+export default UserList
