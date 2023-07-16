2673760ccfb205f9efdc3df9767c78d1affa9b7a
maeta187 <nexus18789@gmail.com>
2023-07-16 18:52:11 +0900

    👔 : UserListのgetAPIの引数付与

--- src/app/users/UserList.tsx
+++ src/app/users/UserList.tsx
@@ -10,7 +10,10 @@ type User = {
  */
 const UserList = async () => {
   // await new Promise((resolve) => setTimeout(resolve, 5000))
-  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  // const response = await fetch('http://localhost:3000/api')
+  const response = await fetch('http://localhost:3000/api?name=John&id=1', {
+    cache: 'no-store'
+  })
   if (!response.ok) throw new Error('Error fetching data')
   const users: User[] = await response.json()
   return (
