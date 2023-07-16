439a2421d89b6532f9aec7d74353eb6fe086f850
maeta187 <nexus18789@gmail.com>
2023-07-16 14:59:55 +0900

    👔 : UserListのErrorHandler追加

--- src/app/users/UserList.tsx
+++ src/app/users/UserList.tsx
@@ -11,6 +11,7 @@ type User = {
 const UserList = async () => {
   await new Promise((resolve) => setTimeout(resolve, 5000))
   const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  if (!response.ok) throw new Error('Error fetching data')
   const users: User[] = await response.json()
   return (
     <ul>
