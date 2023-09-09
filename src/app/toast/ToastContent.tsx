0fb1b592ffe9562e64fb6c2879e3028095af5af6
maeta187 <nexus18789@gmail.com>
2023-09-09 14:16:46 +0900

    👔 : showToastを直接実行するよう修正

--- src/app/toast/ToastContent.tsx
+++ src/app/toast/ToastContent.tsx
@@ -5,33 +5,27 @@ import { useToast } from '@/context/ToastProvider'
 const ToastContent = () => {
   const { showToast, closeToast } = useToast()
 
-  const handleClick = (
-    message: string,
-    type: 'success' | 'warning' | 'error'
-  ) => {
-    showToast(message, type)
-  }
   return (
     <div>
       <div className='text-center w-9/12 my-0 mx-auto pt-36'>
         <button
           className='p-2 border-2 rounded-xl text-green-700 bg-green-200 border-green-700'
           type='button'
-          onClick={() => handleClick('success', 'success')}
+          onClick={() => showToast('success', 'success')}
         >
           success
         </button>
         <button
           className='ml-2 p-2 border-2 rounded-xl text-yellow-700 bg-yellow-200 border-yellow-700'
           type='button'
-          onClick={() => handleClick('warning', 'warning')}
+          onClick={() => showToast('warning', 'warning')}
         >
           warning
         </button>
         <button
           className='ml-2 p-2 border-2 rounded-xl text-red-700 bg-red-200 border-red-700'
           type='button'
-          onClick={() => handleClick('error', 'error')}
+          onClick={() => showToast('error', 'error')}
         >
           error
         </button>
